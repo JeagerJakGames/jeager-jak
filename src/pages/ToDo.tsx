accfa9339b51ca4cb6cba721601933db9cad660f
TJW <jaigerjakgames@gmail.com>
2023-01-31 15:51:42 -0500

    Changed to pull from api

--- src/pages/ToDo.tsx
+++ src/pages/ToDo.tsx
@@ -1,28 +1,26 @@
 import DDLGrid from "../components/Grid/DDLGrid/DDLGrid";
-import Grid from "../components/Grid/Grid";
 
 
-export default function ToDo() {
+export default async function ToDo() {
+    const url = "https://jeagerjak.azurewebsites.net/";
+    const endPoint = "WeatherForecast";
+    let err = null;
+    await fetch(url + endPoint)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        const list = data;
 
-    let i = 0;
-    const list: any[] = [];
-    while (i < 100) {
-        list.push({
-            id: i,
-            name: "test",
-            description: "test",
-            status: "test",
-            priority: "test",
-            created: "test",
-            updated: "test"
-        });
-        i++;
-    }
-
-    return (
-        <div>
-            <h1>ToDo</h1>
-            <DDLGrid name={"todo-list"} gridObjects={list} />
-        </div>
-    )
+        return(
+            <div>
+                <h1>ToDo</h1>
+                <DDLGrid name={"todo-list"} gridObjects={list} />
+            </div>
+        )
+    })
+    .catch((error) => {
+        err = error;
+    });
+    if(err) return <div>{err}</div>
+    return <h1>incomplete</h1>
 }
